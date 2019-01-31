b03a94fdfe0ee679fc3b902fbb2addad2b92e15e
adiltirur <adiltirur@gmail.com>
2019-01-31 12:28:29 +0100

    model file

---
+++ model.py
@@ -0,0 +1,29 @@
+#Facial keypoint tracking  using CNN in pyTorch
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.con1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)
+        #by default the stride and padding will be 1
+        self.conv2 = nn.Conv2d(32,64,5)
+        self.conv3 = nn.Conv2d(64,128,5)
+        self.conv4 = nn.Conv2d(125,256,5)
+
+        self.pool1 = nn.MaxPool2d(2,2)
+
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.fc1 = nn.Linear(11*11*256,1000)
+        self.fc2 = nn.Linear(1000,1000)
+        self.fc3 = nn.Linear(1000, 136)
